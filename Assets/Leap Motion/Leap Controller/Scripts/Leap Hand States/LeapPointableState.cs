c6665be3cc95f2ab78ba19ccfbecb6df7e191947
Dallas Kelle <dkelle@wireless-10-145-199-154.public.utexas.edu>
2014-10-18 19:24:29 -0500

    Sink and room added

---
+++ Assets/Leap Motion/Leap Controller/Scripts/Leap Hand States/LeapPointableState.cs
@@ -0,0 +1,30 @@
+﻿public class LeapPointableState : LeapState{
+	
+	public LeapPointableState() { }
+	
+	public LeapPointableState(LeapGameObject obj)
+	{
+		activeObj = obj;
+	}
+	
+	public override void Enter(HandTypeBase o)
+	{
+		handController = o;
+		if (activeObj)
+			handController.SetActiveObject(activeObj);
+	}
+	
+	public override void Execute()
+	{
+		if (handController.unityHand == null)
+			return;
+		
+		if (handController.activeObj)
+		{
+			// Update active object
+			activeObj.UpdateTransform(handController);
+			
+			CheckEscape();
+		}
+	}
+}
---
+++ Assets/Leap Motion/Leap Controller/Scripts/Leap Objects/LeapPointableObject.cs
@@ -0,0 +1,53 @@
+﻿using Leap;
+
+public class LeapPointableObject : LeapBasicObject{
+	
+	/*
+	public override LeapState Activate(HandTypeBase h)
+	{
+		base.Activate(h);
+		return new LeapPointableState(this);
+	}
+	
+	public override void UpdateTransform (HandTypeBase t)
+	{
+		base.UpdateTransform (t);
+		UpdatePointTransform(t);
+	}
+	
+	void UpdatePointTransform(HandTypeBase t)
+	{
+		Collider o = collider;
+		
+		Vector3 handOffset = new Vector3();
+		Vector3 grabOffsetPos = new Vector3();
+		bool fingerFound = false;
+		
+		// Uses Index Finger to aim
+		if (owner.unityHand.unityFingers.ContainsKey(FINGERS.INDEX))
+		{
+			fingerFound = true;
+		}
+		
+		o.transform.position = owner.transform.position + handOffset - grabOffsetPos;
+		
+		Vector3 tipPosition;
+		
+		if (fingerFound)
+		{
+			tipPosition =
+				owner.unityHand.unityFingers[FINGERS.INDEX].transform.position;
+		}
+		else
+		{
+			tipPosition =
+				owner.unityHand.hand.Fingers.Frontmost.TipPosition.ToUnityTranslated();
+		}
+		
+		//Look at Finger
+		o.transform.LookAt(tipPosition, owner.unityHand.transform.up); // Add finger rotation
+	}
+
+*/
+	
+}
